2c1e1edd4221dee35b931da48af986615a9eb325
gheredia90 <gheredia28@gmail.com>
2017-10-05 13:16:23 +0200

    more use of pipeline operator

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,7 +1,6 @@
 defmodule Dictionary do
   def word_list do
-    contents = File.read!("assets/words.txt")
-    String.split(contents, ~r/\n/)
+    File.read!("assets/words.txt") |> String.split(~r/\n/)
   end
 
   def random_word() do

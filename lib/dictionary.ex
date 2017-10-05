ed6a36142e09f6db408a335bbcc3e18f3823bbf3
gheredia90 <gheredia28@gmail.com>
2017-10-05 13:25:01 +0200

    more pipelines and style

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,9 +1,12 @@
 defmodule Dictionary do
   def word_list do
-    File.read!("assets/words.txt") |> String.split(~r/\n/)
+    "assets/words.txt"
+    |> File.read!
+    |> String.split(~r/\n/)
   end
 
   def random_word() do
-    word_list() |> Enum.random
+    word_list()
+    |> Enum.random
   end
 end

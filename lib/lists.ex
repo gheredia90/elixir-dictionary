6e2ff6bd7340c26e4cc15dcc3c17c2e477dc01a7
gheredia90 <gheredia28@gmail.com>
2017-12-20 00:48:08 +0100

    Added Lists module

---
+++ lib/lists.ex
@@ -0,0 +1,17 @@
+defmodule Lists do
+  def len( [] ), do: 0
+
+  def len([ _head | tail]), do: 1 + len(tail)
+
+  def sum( [] ), do: 0
+
+  def sum([ head | tail]), do: head + sum(tail)
+
+  def square([]), do: []
+
+  def square([ head | tail ]), do: [ head*head | square(tail) ]
+
+  def sum_pairs([]), do: []
+
+  def sum_pairs([ h1, h2 | t]), do: [ h1 + h2 | sum_pairs(t) ]
+end
